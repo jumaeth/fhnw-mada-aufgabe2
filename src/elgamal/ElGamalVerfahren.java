2801a188689dd9f2debd19f98253606a7c9097df
michelscherer <michel.scherer@students.fhnw.ch>
2024-05-20 19:03:36 +0200

    create ElGamalVerfahren

--- src/elgamal/ElGamalVerfahren.java
+++ src/elgamal/ElGamalVerfahren.java
@@ -1,25 +1,4 @@
-package elgamal;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
+package ElGamal;
 
 public class ElGamalVerfahren {
-
-
-    public static void main(String[] args) throws IOException {
-
-        // File which needs to be encoded
-        Path fileToEncode = Path.of("src/ElGamal/text.txt");
-
-        try (InputStream inEncode = Files.newInputStream(fileToEncode)) {
-
-            // Read File which needs to be encoded
-            InputStreamReader inputReader = new InputStreamReader(inEncode);
-            BufferedReader bufferedReader = new BufferedReader(inputReader);
-        }
-    }
 }
